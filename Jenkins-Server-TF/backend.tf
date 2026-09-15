42717f282e4dc6d601043925dbd7fcf559e24967
BhanuS95 <bs.shakya55@gmail.com>
2026-09-16 01:09:27 +0530

    Update backend.tf

--- Jenkins-Server-TF/backend.tf
+++ Jenkins-Server-TF/backend.tf
@@ -3,7 +3,7 @@ terraform {
     bucket         = "my-eks-backend-bhanu"
     region         = "ap-south-1"
     key            = "3-tier-MERN-stack-DevSecOps-Project/Jenkins-Server-TF/terraform.tfstate"
-    dynamodb_table = "Lock-Files"
+    use_lockfile   = true
     encrypt        = true
   }
   required_version = ">=0.13.0"
